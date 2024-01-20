cf5fe11dd456974fc9d84f7829be8c34e835a3b8
ibrahim_ahmad <ibrahimahmad.info@gmail.com>
2024-01-20 17:35:42 +0800

    Add files to create 16x16 grid using js.

---
+++ script.js
@@ -0,0 +1,28 @@
+// set the number of n for nxn grid
+const nth_grid = 16;
+
+// create big container
+let mainContainer = document.createElement('div');
+mainContainer.setAttribute('class', 'mainContainer');
+
+for (let nContainer = 1; nContainer <= nth_grid; nContainer++){
+    
+    let container = document.createElement('div');
+    container.setAttribute('class','container');
+   
+    //add grid to container
+    for (let nGrid = 1; nGrid <= nth_grid; nGrid++){
+        
+        let gridText = `grid_${nContainer}-${nGrid}`;
+        let grid = document.createElement('div');
+        grid.setAttribute('class', 'grid');
+        grid.setAttribute('id', gridText);
+        container.appendChild(grid);
+    }
+
+    //append to main container
+    mainContainer.appendChild(container); 
+};
+
+// append to body element
+document.body.appendChild(mainContainer);
